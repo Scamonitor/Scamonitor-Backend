c323f391358bce0dac74436faecda18ed2292d88
Carlos Paez <carlosalbertopaez2002@gmail.com>
2024-10-25 20:53:59 -0600

    add env variables for gcp

--- api/config.py
+++ api/config.py
@@ -9,3 +9,5 @@ class Config:
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_NAME = os.environ.get('DB_NAME')
+    GCP_CLIENT_EMAIL = os.environ.get('GCP_CLIENT_EMAIL')
+    GCP_PRIVATE_KEY = os.environ.get('GCP_PRIVATE_KEY').replace('\\n', '\n')
